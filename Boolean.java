73569d64b16a1aa45552e1aea2b9e32973eff572
Dennis Yiaile <ykamakei6345@gmail.com>
2020-10-05 16:43:07 +0300

    add java skills

--- Boolean.java
+++ Boolean.java
@@ -1,7 +1,7 @@
 import java.io.Console;
 
 public class Boolean{
- public static void main(String[] args) {
+/* public static void main(String[] args) {
   Console console = System.console();
   System.out.println("Enter your age");
   String stringAge=console.readLine();
@@ -11,5 +11,20 @@ public class Boolean{
 }  else {
   System.out.println("enjoy your self in the club");
 }
+}*/
+public static void main(String[] args) {
+  Console console=System.console();
+  String season="summer";
+  String week="weekend";
+
+  System.out.println("Which day of the period of the week do you like want to visit the us?");
+  String weekDay = console.readLine();
+  System.out.println("which season do you want to make your travel arrangements");
+  String seasonTime = console.readLine();
+  if ((weekDay.equals(week)) && (seasonTime.equals(season))) {
+    System.out.println("your travel arrangements will be expensive");
+  } else {
+    System.out.println("you will probably have an affordable trip");
   }
+}
 }
